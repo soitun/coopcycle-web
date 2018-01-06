b5aaa836f7464f6f066bb25c63ef1cb13a26c809
Alexandre Segura <mex.zktk@gmail.com>
2018-01-06 16:17:35 +0100

    Use postcss-loader in Webpack, remove prefixes.

--- webpack.config.js
+++ webpack.config.js
@@ -61,12 +61,15 @@ var webpackConfig = {
           test: /\.scss$/,
           loader: ExtractTextPlugin.extract({
             fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
+            use: ['css-loader', 'postcss-loader', 'sass-loader']
           })
       },
       {
           test: /\.css$/,
-          loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader']
+          })
       },
       {
           test: /\.(eot|ttf|woff|woff2)$/,
