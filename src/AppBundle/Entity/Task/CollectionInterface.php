f09a09a2c5fcc76818c53eabedda60b682f4b220
Alexandre Segura <mex.zktk@gmail.com>
2018-02-25 15:41:37 +0100

    Introduce Task\CollectionInterface.

---
+++ src/AppBundle/Entity/Task/CollectionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Entity\Task;
+
+use AppBundle\Entity\Task;
+
+interface CollectionInterface
+{
+    public function getDistance();
+
+    public function getDuration();
+}
