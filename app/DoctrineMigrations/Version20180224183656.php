db9b479a0a15236b71d1c6d7596a04bd517ab16f
Alexandre Segura <mex.zktk@gmail.com>
2018-02-25 15:47:23 +0100

    Introduce order.readyAt.

---
+++ app/DoctrineMigrations/Version20180224183656.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180224183656 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE order_ ADD ready_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('UPDATE order_ SET ready_at = CURRENT_TIMESTAMP');
+        $this->addSql('ALTER TABLE order_ ALTER ready_at SET NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE order_ DROP ready_at');
+    }
+}
