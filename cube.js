d230b3350c4ebde84adb9ee731a28104a7df4052
Alexandre Segura <mex.zktk@gmail.com>
2026-02-23 12:05:28 +0100

    Return drivers as objects, fix scheduledRefreshContexts.

--- cube.js
+++ cube.js
@@ -1,9 +1,6 @@
 // https://cube.dev/docs/multitenancy-setup#multiple-db-instances-with-same-schema
 // https://cube.dev/docs/product/configuration/advanced/multitenancy#multiple-db-instances-with-same-data-model
 
-const PostgresDriver = require('@cubejs-backend/postgres-driver');
-const ClickhouseDriver = require('@cubejs-backend/clickhouse-driver');
-
 module.exports = {
   extendContext: (req) => {
     return {
@@ -19,19 +16,21 @@ module.exports = {
   driverFactory: ({ securityContext, dataSource }) => {
 
     if (dataSource === 'clickhouse') {
-
-      return new ClickhouseDriver({
+      return {
+        type: 'clickhouse',
         host: 'clickhouse',
-        port: 8123,
+        port: '8123',
         database: 'coopcycle',
-        user: 'coopcycle',
+        username: 'coopcycle',
         password: 'coopcycle',
-      })
+        ssl: false,
+      }
     }
 
-    return new PostgresDriver({
-      database: `${securityContext && securityContext.database ? securityContext.database : 'coopcycle'}`,
-    })
+    return {
+      type: 'postgres',
+      database: `${securityContext?.database || 'coopcycle'}`,
+    }
   },
 
 
@@ -41,7 +40,7 @@ module.exports = {
       securityContext: {
         database: 'coopcycle',
         base_url: 'http://nginx',
-        s3_path: '/images/exports/%type%'
+        instance: 'default'
       },
     },
   ],
