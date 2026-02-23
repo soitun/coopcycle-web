b7983a35d5deca0740f8f6c22538c0231db87437
Alexandre Segura <mex.zktk@gmail.com>
2026-02-23 10:07:45 +0100

    Setup Clickhouse in Docker.

--- cube.js
+++ cube.js
@@ -1,17 +1,32 @@
 // https://cube.dev/docs/multitenancy-setup#multiple-db-instances-with-same-schema
+// https://cube.dev/docs/product/configuration/advanced/multitenancy#multiple-db-instances-with-same-data-model
 
 const PostgresDriver = require('@cubejs-backend/postgres-driver');
-const DuckDbDriver = require('@cubejs-backend/duckdb-driver');
+const ClickhouseDriver = require('@cubejs-backend/clickhouse-driver');
 
 module.exports = {
+  extendContext: (req) => {
+    return {
+      securityContext: {
+        ...req.securityContext,
+      }
+    }
+  },
   contextToAppId: ({ securityContext }) =>
-    `CUBEJS_APP_${securityContext && securityContext.database ? securityContext.database : 'coopcycle'}`,
+    `CUBEJS_APP_${securityContext?.database || 'coopcycle'}`,
   contextToOrchestratorId: ({ securityContext }) =>
-    `CUBEJS_APP_${securityContext && securityContext.database ? securityContext.database : 'coopcycle'}`,
+    `CUBEJS_APP_${securityContext?.database || 'coopcycle'}`,
   driverFactory: ({ securityContext, dataSource }) => {
 
-    if (dataSource === 'duckdb') {
-      return new DuckDbDriver({})
+    if (dataSource === 'clickhouse') {
+
+      return new ClickhouseDriver({
+        host: 'clickhouse',
+        port: 8123,
+        database: 'coopcycle',
+        username: 'coopcycle',
+        password: 'coopcycle',
+      })
     }
 
     return new PostgresDriver({
