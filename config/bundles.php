5fd868c048a2fb0c0e018238338c7d81848936e2
Alexandre Segura <mex.zktk@gmail.com>
2026-02-24 08:06:08 +0100

    Install Symfony UX Autocomplete.

--- config/bundles.php
+++ config/bundles.php
@@ -73,4 +73,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
 ];
